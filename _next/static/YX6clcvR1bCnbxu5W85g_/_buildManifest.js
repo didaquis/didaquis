af9101c38547ecfbbebae48896883e154cbe580a
Dídac García <didaquis@gmail.com>
2020-11-23 00:17:29 +0100

    Content has been updated

---
+++ _next/static/YX6clcvR1bCnbxu5W85g_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-da06f0c2b5e62f7fc032.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
