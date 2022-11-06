f6c9ecb97c3ca00e46a1821982700985b31aa18c
Dídac García <didaquis@gmail.com>
2022-11-07 00:05:09 +0100

    Update content

--- _next/static/YX6clcvR1bCnbxu5W85g_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-da06f0c2b5e62f7fc032.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fa39c7ce12d3ae2cec40.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
